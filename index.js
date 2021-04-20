19bd70cbda2bb1ae7ff103931fbffc58f58ced79
Melanie W <macbookair@Melanie-W.local>
2021-04-19 21:44:41 -0600

    change message:

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-	res.send({ hi: 'there'});
+	res.send({ hi: 'ugly tuan who breaks my heart all the time'});
 });
 
 // set the port of our application
